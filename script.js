74032b30884d854013d3f5c5a6958541138f3256
Paul Dredge <dredgos@googlemail.com>
2021-06-08 14:39:03 +0100

    Amended the li to ensure the correct name was added for each date

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ let christmasDay = new Date(2021,12,25);
 let newYearsDay = new Date(2022,01,01);
 let valentinesDay = new Date(2022,02,14);
 let importantDates = [christmasDay, newYearsDay, valentinesDay];
+let dateNames = ["Christmas", "New Years", "Valentines"];
 let differenceInMs = (date) => (date-today);
 let convertMsToDays = (ms) => (ms/(24*3600*1000));
 let dateDifferences = importantDates.map(date => parseInt(convertMsToDays(differenceInMs(date)).toFixed(0)))
 
 
-  $("#trigger").click(function(){
-    dateDifferences.map(difference => $("#days-until").append("<li>" + difference.toString()+" days until Valentines Day</li>"))
-    
-  })
+$("#trigger").click(function(){
+    dateDifferences.map((difference, index) => $("#days-until").append(`<li> ${difference.toString()} days until ${dateNames[index]} Day</li>`));    
+})
+
 
