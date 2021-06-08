5f296557e2a5a6834de526c62a35e49457f056a4
Paul Dredge <dredgos@googlemail.com>
2021-06-08 14:48:48 +0100

    Removed intermediate variables for each individual date

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 let today = new Date();
-let christmasDay = new Date(2021,12,25);
-let newYearsDay = new Date(2022,01,01);
-let valentinesDay = new Date(2022,02,14);
-let importantDates = [christmasDay, newYearsDay, valentinesDay];
+let importantDates = [new Date(2021,12,25), new Date(2022,01,01), new Date(2022,02,14)];
 let dateNames = ["Christmas", "New Years", "Valentines"];
+
 let differenceInMs = (date) => (date-today);
 let convertMsToDays = (ms) => (ms/(24*3600*1000));
+
 let dateDifferences = importantDates.map(date => parseInt(convertMsToDays(differenceInMs(date)).toFixed(0)))
 
 
