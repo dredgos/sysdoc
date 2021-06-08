e481a35e22d317e5ead8dea5b50b6a6c32df7e6f
Paul Dredge <dredgos@googlemail.com>
2021-06-08 14:23:30 +0100

    Refactored to map over dates to get differences, then map over differences to produce the LI

--- script.js
+++ script.js
@@ -3,24 +3,13 @@ let christmasDay = new Date(2021,12,25);
 let newYearsDay = new Date(2022,01,01);
 let valentinesDay = new Date(2022,02,14);
 let importantDates = [christmasDay, newYearsDay, valentinesDay];
-
 let differenceInMs = (date) => (date-today);
 let convertMsToDays = (ms) => (ms/(24*3600*1000));
+let dateDifferences = importantDates.map(date => parseInt(convertMsToDays(differenceInMs(date)).toFixed(0)))
 
-$("#trigger").click(function(){
-    var daysDifference = parseInt(convertMsToDays(differenceInMs(christmasDay)).toFixed(0));
-    
-    $("#days-until").append("<li>" + daysDifference.toString()+" days until Christmas</li>");
-  })
-  
+
   $("#trigger").click(function(){
-    var daysDifference = parseInt(convertMsToDays(differenceInMs(newYearsDay)).toFixed(0));
+    dateDifferences.map(difference => $("#days-until").append("<li>" + difference.toString()+" days until Valentines Day</li>"))
     
-    $("#days-until").append("<li>" + daysDifference.toString()+" days until New Year</li>");
   })
-  
-  $("#trigger").click(function(){
-    var daysDifference = parseInt(convertMsToDays(differenceInMs(valentinesDay)).toFixed(0));
-    
-    $("#days-until").append("<li>" + daysDifference.toString()+" days until Valentines Day</li>");
-  })
+
