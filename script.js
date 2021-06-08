d609c24ceeded52b4218c77f49e98548530d6b10
Paul Dredge <dredgos@googlemail.com>
2021-06-08 13:56:44 +0100

    Create singe variables in global space to be used by all functions

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
+let today = new Date();
+let christmasDay = new Date(2021,12,25);
+let newYearsDay = new Date(2022,01,01);
+let valentinesDay = new Date(2022,02,14);
+let importantDates = [christmasDay, newYearsDay, valentinesDay];
+
 $("#trigger").click(function(){
-    var today = new Date();
-    var christmasDay = new Date(2021,12,25)
     var daysDifference = parseInt((christmasDay-today)/(24*3600*1000));
     
     $("#days-until").append("<li>" + daysDifference.toString()+" days until Christmas</li>");
   })
   
   $("#trigger").click(function(){
-    var today = new Date();
-    var newYearsDay = new Date(2022,01,01)
     var daysDifference = parseInt((newYearsDay-today)/(24*3600*1000));
     
     $("#days-until").append("<li>" + daysDifference.toString()+" days until New Year</li>");
   })
   
   $("#trigger").click(function(){
-    var today = new Date();
-    var valentinesDay = new Date(2022,02,14)
     var daysDifference = parseInt((valentinesDay-today)/(24*3600*1000));
     
     $("#days-until").append("<li>" + daysDifference.toString()+" days until Valentines Day</li>");
