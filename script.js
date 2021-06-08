451587f22c84b2d887ad2eb122dfab7fc03760fc
Paul Dredge <dredgos@googlemail.com>
2021-06-08 14:01:03 +0100

    Created functions for calculating difference in ms & converting ms to days

--- script.js
+++ script.js
@@ -4,20 +4,23 @@ let newYearsDay = new Date(2022,01,01);
 let valentinesDay = new Date(2022,02,14);
 let importantDates = [christmasDay, newYearsDay, valentinesDay];
 
+let differenceInMs = (date) => (date-today);
+let convertMsToDays = (ms) => (ms/(24*3600*1000));
+
 $("#trigger").click(function(){
-    var daysDifference = parseInt((christmasDay-today)/(24*3600*1000));
+    var daysDifference = parseInt(convertMsToDays(differenceInMs(christmasDay)).toFixed(0));
     
     $("#days-until").append("<li>" + daysDifference.toString()+" days until Christmas</li>");
   })
   
   $("#trigger").click(function(){
-    var daysDifference = parseInt((newYearsDay-today)/(24*3600*1000));
+    var daysDifference = parseInt(convertMsToDays(differenceInMs(newYearsDay)).toFixed(0));
     
     $("#days-until").append("<li>" + daysDifference.toString()+" days until New Year</li>");
   })
   
   $("#trigger").click(function(){
-    var daysDifference = parseInt((valentinesDay-today)/(24*3600*1000));
+    var daysDifference = parseInt(convertMsToDays(differenceInMs(valentinesDay)).toFixed(0));
     
     $("#days-until").append("<li>" + daysDifference.toString()+" days until Valentines Day</li>");
   })
