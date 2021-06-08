aff3a618167e9c70b0c3c710b38b28fc16ab65db
Paul Dredge <dredgos@googlemail.com>
2021-06-08 13:54:23 +0100

    Renamed variables to descriptive names

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 $("#trigger").click(function(){
-    var apples = new Date();
-    var oranges = new Date(2021,12,25)
-    var pears = parseInt((oranges-apples)/(24*3600*1000));
+    var today = new Date();
+    var christmasDay = new Date(2021,12,25)
+    var daysDifference = parseInt((christmasDay-today)/(24*3600*1000));
     
-    $("#days-until").append("<li>" + pears.toString()+" days until Christmas</li>");
+    $("#days-until").append("<li>" + daysDifference.toString()+" days until Christmas</li>");
   })
   
   $("#trigger").click(function(){
-    var apples = new Date();
-    var oranges = new Date(2022,01,01)
-    var pears = parseInt((oranges-apples)/(24*3600*1000));
+    var today = new Date();
+    var newYearsDay = new Date(2022,01,01)
+    var daysDifference = parseInt((newYearsDay-today)/(24*3600*1000));
     
-    $("#days-until").append("<li>" + pears.toString()+" days until New Year</li>");
+    $("#days-until").append("<li>" + daysDifference.toString()+" days until New Year</li>");
   })
   
   $("#trigger").click(function(){
-    var apples = new Date();
-    var oranges = new Date(2022,02,14)
-    var pears = parseInt((oranges-apples)/(24*3600*1000));
+    var today = new Date();
+    var valentinesDay = new Date(2022,02,14)
+    var daysDifference = parseInt((valentinesDay-today)/(24*3600*1000));
     
-    $("#days-until").append("<li>" + pears.toString()+" days until Valentines Day</li>");
+    $("#days-until").append("<li>" + daysDifference.toString()+" days until Valentines Day</li>");
   })
